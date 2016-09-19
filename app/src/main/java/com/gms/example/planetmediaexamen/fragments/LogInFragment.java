cb0ddc736b7a14c48ffde9e0b4e9b87b14deddc2
vladimir.saldivar <vsaldivar@appwhere.mx>
2016-09-19 16:58:51 -0500

    Initial commit

---
+++ app/src/main/java/com/gms/example/planetmediaexamen/fragments/LogInFragment.java
@@ -0,0 +1,172 @@
+package com.gms.example.planetmediaexamen.fragments;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.gms.example.planetmediaexamen.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link LogInFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link LogInFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LogInFragment extends Fragment implements View.OnClickListener {
+    private EditText editTextUsuario,editTextPassword;
+    private Button btningresar;
+    private View rootView;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public LogInFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment LogInFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static LogInFragment newInstance(String param1, String param2) {
+        LogInFragment fragment = new LogInFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        rootView = inflater.inflate(R.layout.fragment_log_in, container, false);
+        editTextUsuario = (EditText) rootView.findViewById(R.id.editTextUsuario);
+        editTextPassword = (EditText) rootView.findViewById(R.id.editTextPassword);
+        btningresar = (Button) rootView.findViewById(R.id.btningresar);
+        btningresar.setOnClickListener(this);
+
+        return rootView;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btningresar){
+            JSONObject json = new JSONObject();
+            try {
+                json.put("usuario", editTextUsuario.getText().toString());
+                json.put("password", editTextPassword.getText().toString());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            activateUser(json);
+
+        }
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+    public void activateUser(JSONObject gson) {
+        //SPARTDARK
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
+                getString(R.string.loginurl), gson,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Snackbar.make(rootView, "Bienvenid@", Snackbar.LENGTH_SHORT).show();
+                        Log.i("MYResponse",response.toString());
+                    }
+                }
+                , new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Snackbar.make(rootView, "Ocurrio un error", Snackbar.LENGTH_LONG).show();
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
+        requestQueue.add(jsonObjReq);
+    }
+}
